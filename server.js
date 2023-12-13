9e97e0408d4aa80261c79323889a78edf72843dc
JoJava25 <trogdor2k5@hotmail.com>
2023-12-12 21:30:56 -0500

    Beginning process to make project full stack

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+
+//Middleware
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors())
+app.use(express.static('public'))
+
