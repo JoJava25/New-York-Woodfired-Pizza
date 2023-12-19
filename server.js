2364b7fd2d68b42b96b48d5adcc56ee7fb4271dc
JoJava25 <trogdor2k5@hotmail.com>
2023-12-18 21:21:48 -0500

    Set up ports and error message for server

--- server.js
+++ server.js
@@ -13,3 +13,15 @@ app.use(express.static('public'))
 
 //Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', ()=> {
+    console.log("Connected to MongoDB")
+})
+
+//Start server
+const PORT = 8000
+app.listen(PORT, () => {
+    console.log(`Server connceted on ${PORT}`)
+})
